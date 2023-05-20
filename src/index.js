a1154fbf6764e4e9dc8e6dc16a16d9631665abc0
harumy08 <harumy08@gmail.com>
2023-05-20 16:21:57 -0600

    React Dom is replaced by createRoot and Babel is integrated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-//import { React } from 'react';
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './routes/App';
 
-const rootElement = document.getElementById('app');
-const root = createRoot(rootElement);
-
-root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+const container = document.getElementById('app');
+const root = createRoot(container); 
+root.render(<App tab="home" />);
